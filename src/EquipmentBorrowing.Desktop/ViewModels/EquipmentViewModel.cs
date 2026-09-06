6029d19b87c18de3759b07f0ab66c461c2da280c
App_dev_dalman <andredalman14@gmail.com>
2026-09-06 14:40:39 +0800

    Add equipment view and bindings

---
+++ src/EquipmentBorrowing.Desktop/ViewModels/EquipmentViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using EquipmentBorrowing.Domain;
+
+namespace EquipmentBorrowing.Desktop.ViewModels;
+
+public partial class EquipmentViewModel : ViewModelBase
+{
+    public ObservableCollection<Equipment> Equipment { get; } = new();
+    public ObservableCollection<Student> Students { get; } = new();
+
+    [ObservableProperty] private Equipment? selectedEquipment;
+    [ObservableProperty] private Student? selectedStudent;
+    [ObservableProperty] private DateTimeOffset? expectedReturnDate = DateTimeOffset.Now.AddDays(3);
+    [ObservableProperty] private string? statusMessage;
+}
